6388f735ede5b19e24f7c9f0c6ba99a240df34be
eskrobo1 <eskrobo1@etf.unsa.ba>
2025-08-31 19:19:06 +0000

    new best model

--- src/train.py
+++ src/train.py
@@ -147,7 +147,7 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Train
-    model = train_model(model, train_loader, val_loader, criterion, optimizer, device, epochs=100)
+    model = train_model(model, train_loader, val_loader, criterion, optimizer, device, epochs=1000)
 
     # Test -> koristi najbolji model ako postoji
     best_ckpt = os.path.join("checkpoints", "best_model.pth")
